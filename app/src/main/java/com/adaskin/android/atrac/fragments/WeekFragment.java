985b2444d1d434af8bd9e5c29d40ed82b088773a
dadaskin <dave@adaskin.com>
2017-07-16 18:57:27 -0700

    Build 2.0.6 Dummy data appears in swipable view.

--- app/src/main/java/com/adaskin/android/atrac/fragments/WeekFragment.java
+++ app/src/main/java/com/adaskin/android/atrac/fragments/WeekFragment.java
@@ -5,11 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.adaskin.android.atrac.R;
 import com.adaskin.android.atrac.WeeklyActivity;
+import com.adaskin.android.atrac.models.DailyEntry;
 import com.adaskin.android.atrac.models.WorkWeek;
+
+import java.util.ArrayList;
 
 
 public class WeekFragment extends Fragment {
@@ -19,14 +24,6 @@ public class WeekFragment extends Fragment {
 
     public WeekFragment() {
     }
-
-//    public static WeekFragment newInstance(int sectionNumber) {
-//        WeekFragment fragment = new WeekFragment();
-//        Bundle args = new Bundle();
-//        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
 
     public static WeekFragment newInstance(WeeklyActivity.SectionsPagerAdapter pager, int weekIndex) {
         WeekFragment fragment = new WeekFragment();
@@ -41,14 +38,39 @@ public class WeekFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
         View rootView = inflater.inflate(R.layout.fragment_weekly, container, false);
-        TextView textView = (TextView)rootView.findViewById(R.id.week_section_label);
-
-        //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
 
         int weekIndex = getArguments().getInt(WEEK_INDEX_KEY);
         WorkWeek thisWeek = mPager.mWeekCollection.get(weekIndex);
+        ArrayList<String> thisWeekAsArrayList = convertToArrayList(thisWeek);
 
+        TextView textView = (TextView)rootView.findViewById(R.id.week_section_label);
         textView.setText(thisWeek.mStartDateString);
+
+        GridView gridView = (GridView)rootView.findViewById(R.id.weekly_grid);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, thisWeekAsArrayList);
+        gridView.setAdapter(adapter);
+
         return rootView;
     }
+
+    private static ArrayList<String> convertToArrayList(WorkWeek ww) {
+        ArrayList<String> result = new ArrayList<>();
+
+        addDailyEntryToResult(result, ww.mMonday);
+        addDailyEntryToResult(result, ww.mTuesday);
+        addDailyEntryToResult(result, ww.mWednesday);
+        addDailyEntryToResult(result, ww.mThursday);
+        addDailyEntryToResult(result, ww.mFriday);
+
+        return result;
+    }
+
+    private static void addDailyEntryToResult(ArrayList<String> list, DailyEntry de) {
+        list.add("Mon\nApr17");
+        list.add("08:00");
+        list.add("12:00");
+        list.add("13:00");
+        list.add("17:00");
+    }
+
 }
